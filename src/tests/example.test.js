9cea91710ac9ccdfabd03bcaa0e2dfd5b08e506c
Yikai Zhang <zhangyikai@hotmail.com>
2019-07-09 18:44:51 -0400

    add example test file

--- src/tests/example.test.js
+++ src/tests/example.test.js
@@ -4,3 +4,10 @@ test("add two numbers", () => {
   const result = add(3, 4);
   expect(result).toBe(7);
 });
+
+const generateGreeting = name => `Hello, ${name}!`;
+
+test("add name into a string", () => {
+  const result = generateGreeting("Mike");
+  expect(result).toBe("Hello, Mike!");
+});
